9061066be5f85b3cd422c5463a9e9b8656ad681f
fauziardha1 <fauziardasaputra@gmail.com>
2023-01-21 02:42:38 +0700

    initial project

---
+++ MovieApp-VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp-VIPER
+//
+//  Created by FauziArda on 21/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
